b6352227a05933632ffca3149311dbac879bca35
Kleber Pinel <kleberpinel@gmail.com>
2013-09-12 14:10:25 -0300

    Initial Commit

---
+++ Matchsimo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchsimo
+//
+//  Created by Kleber Pinel Bernardo da Silva on 9/12/13.
+//  Copyright (c) 2013 Kleber Pinel Bernardo da Silva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchsimo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchsimo
+//
+//  Created by Kleber Pinel Bernardo da Silva on 9/12/13.
+//  Copyright (c) 2013 Kleber Pinel Bernardo da Silva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
